0f2ce454ac167e88ba3e2c7d9e573b6dca36cb91
Luis Maia <lipalupa1@gmail.com>
2026-04-30 17:07:12 -0300

    articles table created, article controller and service created also added relational attributes to the article class

---
+++ WikiDK/Controllers/ArticleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WikiDK.Services;
+
+namespace WikiDK.Controllers
+{
+    [Route("articles")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly ArticleService _articleService;
+
+        public ArticleController(ArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+    }
+}
---
+++ WikiDK/Services/ArticleService.cs
@@ -0,0 +1,54 @@
+﻿using WikiDK.Objects;
+using WikiDK.Repositories;
+
+namespace WikiDK.Services
+{
+    public class ArticleService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ArticleService(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<Article> Publish(string title, string content, int authorId)
+        {
+            // Create a new article
+            Article article = new()
+            {
+                Title = title,
+                Content = content,
+                Created = DateTime.Now,
+                Updated = DateTime.Now,
+                AuthorId = authorId
+            };
+            // Add article to the database and save changes
+            _dbContext.Articles.Add(article);
+            await _dbContext.SaveChangesAsync();
+            return article;
+        }
+
+        public async Task<Article?> GetById(int id)
+        {
+            return await _dbContext.Articles.FindAsync(id);
+        }
+
+        public async Task Update(int id, string title, string content, int authorId)
+        {
+            var article = await GetById(id) ?? throw new Exception("Article not found");
+            await Update(article, title, content, authorId);
+        }
+
+        public async Task Update(Article article, string title, string content, int authorId)
+        {          
+            article.Title = title;
+            article.Content = content;
+            article.AuthorId = authorId;
+            article.Updated = DateTime.Now;
+
+            _dbContext.Update(article);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
