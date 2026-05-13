07113383f90c912b74e4b6c3a6da6fed0d987235
Luis Maia <lipalupa1@gmail.com>
2026-05-13 13:54:38 -0300

    added former ServerSettings (now ServerDefaults) to tracking

---
+++ WikiDK/ServerSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace WikiDK
+{
+    public static class ServerSettings
+    {
+        public static string DefaultUserIcon { get; set; } = "https://cdn-icons-png.flaticon.com/512/149/149071.png";
+    }
+}
