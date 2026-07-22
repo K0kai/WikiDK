eeff852b5c28e532dbde9835b6b9e6802dda430d
Luis Maia <lipalupa1@gmail.com>
2026-07-22 16:23:01 -0300

    added: policy.cs class added: permission.cs class

---
+++ WikiDK/Objects/Policy.cs
@@ -0,0 +1,11 @@
+﻿namespace WikiDK.Objects
+{
+    public class Policy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTimeOffset CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTimeOffset UpdatedAt { get; private set; } = DateTime.UtcNow;
+    }
+}
