58383dd2c13ccbebf6e491130dff119422fc266f
Luis Maia <lipalupa1@gmail.com>
2026-07-22 16:29:31 -0300

    added role.cs class

---
+++ WikiDK/Objects/Role.cs
@@ -0,0 +1,13 @@
+﻿namespace WikiDK.Objects
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTimeOffset CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTimeOffset UpdatedAt { get; set; } = DateTime.UtcNow;
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+}
