21d1c7271f52df3f45303e3a10a6d022aee0edc5
Luis Maia <lipalupa1@gmail.com>
2026-07-22 16:26:03 -0300

    added role_policy.cs class

---
+++ WikiDK/Objects/Role_Policy.cs
@@ -0,0 +1,9 @@
+﻿namespace WikiDK.Objects
+{
+    public class Role_Policy
+    {
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public int PolicyId { get; set; }
+    }
+}
