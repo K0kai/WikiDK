21df010730f0315a92783dc7c712436da723eacf
Luis Maia <lipalupa1@gmail.com>
2026-07-22 16:25:15 -0300

    added policy_permission.cs class

---
+++ WikiDK/Objects/Policy_Permission.cs
@@ -0,0 +1,9 @@
+﻿namespace WikiDK.Objects
+{
+    public class Policy_Permission
+    {
+        public int Id { get; set; }
+        public int PolicyId { get; set; }
+        public int PermissionId { get; set; }
+    }
+}
